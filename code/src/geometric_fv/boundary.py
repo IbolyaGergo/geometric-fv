13f3d3cbbc937a9c049d5b17abb448dc19c2cc44
IbolyaGergo <ibolyagergi@gmail.com>
2026-02-13 22:41:26 +0100

    Add implicit quasi-periodic bc support

--- code/src/geometric_fv/boundary.py
+++ code/src/geometric_fv/boundary.py
@@ -8,19 +8,21 @@ class BCType(Enum):
 
 def _apply_bc_constant_extend(u_old: np.ndarray,
                               u_new: np.ndarray,
-                              nghost: int):
+                              nghost: int,
+                              clf: float):
     # BC
     u_old[0] = u_old[1]
     u_old[-1] = u_old[-2]
     u_new[0] = u_old[0]
+    u_new[-1] = u_old[-1]
 
 # nghost = 1
-# 0 || 1 | 2 | ... | -2 || -1
+# 0 \\ 1 \ 2 \ ... \ -2 \\ -1
 # for i in [0]:
 #     u[0] = u[-2]
 #     u[-1] = u[1]
 # nghost = 2
-# 0 | 1 || 2 | 3 | ... \ -3 || -2 | -1
+# 0 \ 1 \\ 2 \ 3 \ ... \ -3 \\ -2 \ -1
 # for i in [0, 1]:
 #     u[0] = u[-4]
 #     u[1] = u[-3]
@@ -28,10 +30,21 @@ def _apply_bc_constant_extend(u_old: np.ndarray,
 #     u[-2] = u[2]
 def _apply_bc_quasi_periodic(u_old: np.ndarray,
                              u_new: np.ndarray,
-                             nghost: int):
+                             nghost: int,
+                             cfl: float):
+
     for i in range(nghost):
         u_old[i] = u_old[-2*nghost + i]
         u_old[-1 - i] = u_old[2*nghost - 1 - i]
+
+    if cfl > 0.0:
+        u_new[nghost - 1] = \
+                (1 + int(np.floor(cfl)) - cfl) * u_old[-2 - int(np.floor(cfl))] + \
+                    (cfl - int(np.floor(cfl))) * u_old[-3 - int(np.floor(cfl))]
+    else:
+        u_new[-1] = \
+                (1 + int(np.floor(-cfl))- (-cfl)) * u_old[1 + int(np.floor(-cfl))] + \
+                (-cfl - int(np.floor(-cfl))) * u_old[2 + int(np.floor(-cfl))]
 
 _apply_bc_types = {
         BCType.CONSTANT_EXTEND: _apply_bc_constant_extend,
@@ -41,9 +54,10 @@ _apply_bc_types = {
 def apply_bc(bc_type: BCType,
              u_old: np.ndarray,
              u_new: np.ndarray,
-             nghost: int):
+             nghost: int,
+             cfl: float):
+
     apply_bc_func = _apply_bc_types.get(bc_type)
     if apply_bc_func is None:
         raise ValueError(f"Unsupported BC type: {bc_type}")
-    return apply_bc_func(u_old, u_new, nghost)
-
+    return apply_bc_func(u_old, u_new, nghost, cfl)
--- code/test/test_boundary.py
+++ code/test/test_boundary.py
@@ -11,14 +11,14 @@ ncells = mesh.ncells
 
 u0 = np.sin(2*np.pi*x_c)
 
+bc_type = BCType.QUASI_PERIODIC
 
 @pytest.mark.parametrize("nghost", [1, 2])
 def test_apply_bc_quasi_periodic_nghost_1_2(nghost):
     u_old = np.pad(u0, (nghost,nghost), 'constant', constant_values=0.0)
     u_new = np.copy(u_old)
 
-    bc_type = BCType.QUASI_PERIODIC
-    apply_bc(bc_type, u_old, u_new, nghost)
+    apply_bc(bc_type, u_old, u_new, nghost, cfl=0.0)
 
     if nghost == 1:
         # 0 \\ 1 \ 2 \ ... \ -2 \\ -1
@@ -31,3 +31,34 @@ def test_apply_bc_quasi_periodic_nghost_1_2(nghost):
         assert u_old[-2] == u_old[2]
         assert u_old[-1] == u_old[3]
 
+@pytest.mark.parametrize("cfl", [0.6, 1.5, 2.7])
+def test_apply_bc_quasi_periodic_cfl(cfl):
+    nghost = 1
+    # 0 \\ 1 \ 2 \ ... \ -2 \\ -1
+
+    u_old = np.pad(u0, (nghost,nghost), 'constant', constant_values=0.0)
+    u_new = np.copy(u_old)
+
+    apply_bc(bc_type, u_old, u_new, nghost, cfl)
+    if (cfl >= 0.0 and cfl <= 1.0):
+        assert pytest.approx(u_new[0]) == (1 - cfl) * u_old[-2] + cfl * u_old[-3]
+    elif (cfl >= 1.0 and cfl <= 2.0):
+        assert pytest.approx(u_new[0]) == (2 - cfl) * u_old[-3] + (cfl - 1) * u_old[-4]
+    elif (cfl >= 2.0 and cfl <= 3.0):
+        assert pytest.approx(u_new[0]) == (3 - cfl) * u_old[-4] + (cfl - 2) * u_old[-5]
+
+@pytest.mark.parametrize("cfl", [-0.6, -1.5, -2.7])
+def test_apply_bc_quasi_periodic_negative_cfl(cfl):
+    nghost = 1
+    # 0 \\ 1 \ 2 \ ... \ -2 \\ -1
+
+    u_old = np.pad(u0, (nghost,nghost), 'constant', constant_values=0.0)
+    u_new = np.copy(u_old)
+
+    apply_bc(bc_type, u_old, u_new, nghost, cfl)
+    if (-cfl >= 0.0 and -cfl <= 1.0):
+        assert pytest.approx(u_new[-1]) == (1 - (-cfl)) * u_old[1] + (-cfl) * u_old[2]
+    if (-cfl >= 1.0 and -cfl <= 2.0):
+        assert pytest.approx(u_new[-1]) == (2 - (-cfl)) * u_old[2] + (-cfl - 1) * u_old[3]
+    if (-cfl >= 2.0 and -cfl <= 3.0):
+        assert pytest.approx(u_new[-1]) == (3 - (-cfl)) * u_old[3] + (-cfl - 2) * u_old[4]
