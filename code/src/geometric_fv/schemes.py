0b8b1f515b3cd02176a96752da7f887dd834f89e
IbolyaGergo <ibolyagergi@gmail.com>
2026-02-26 22:23:40 +0100

    Use clearer notation for cell values
    In schemes.py, change
    u_old_i = state.u_old[i]
    u_old_im1 = state.u_old[i - 1]
    ...
    to
    u_old = state.u_old
    u_new = state.u_new
    ...

--- code/src/geometric_fv/schemes.py
+++ code/src/geometric_fv/schemes.py
@@ -25,15 +25,14 @@ class SecondOrderImplicit(Scheme):
     maxiter: int = 50
 
     def _update_cell_guess(self, state: SolverState, i: int) -> float:
-        u_old_i = state.u_old[i]
-        u_old_im1 = state.u_old[i - 1]
-        u_new_im1 = state.u_new[i - 1]
+        u_old = state.u_old
+        u_new = state.u_new
         cfl = state.cfl
 
         coeff = (1 - cfl) / (1 + cfl)
-        u_new_i_guess = coeff * u_old_i + u_old_im1 - coeff * u_new_im1
+        u_new_i_guess = coeff * u_old[i] + u_old[i - 1] - coeff * u_new[i - 1]
         if self.limiter_type is not LimiterType.NONE:
-            u_new_i_guess = np.median([u_new_i_guess, u_old_i, u_new_im1])
+            u_new_i_guess = np.median([u_new_i_guess, u_old[i], u_new[i - 1]])
 
         return u_new_i_guess
 
@@ -43,23 +42,23 @@ class SecondOrderImplicit(Scheme):
         state: SolverState,
         i: int,
     ) -> float:
-        slope_i = compute_slope(
+        u_old = state.u_old
+        u_new = state.u_new
+        slope = state.slope
+
+        cfl = state.cfl
+
+        slope[i] = compute_slope(
             state,
             i=i,
             u_new_i=u_new_i_current,
             slope_type=self.slope_type,
             limiter_type=self.limiter_type,
         )
-        state.slope[i] = slope_i
-
-        u_old_i = state.u_old[i]
-        u_new_im1 = state.u_new[i - 1]
-        slope_im1 = state.slope[i - 1]
-        cfl = state.cfl
 
         # fmt: off
-        u_new_i_next = (u_old_i + cfl * u_new_im1) / (1.0 + cfl) \
-                - 0.5 * cfl * (state.slope[i] - slope_im1)
+        u_new_i_next = (u_old[i] + cfl * u_new[i - 1]) / (1.0 + cfl) \
+                - 0.5 * cfl * (slope[i] - slope[i - 1])
         # fmt: on
         return u_new_i_next
 
