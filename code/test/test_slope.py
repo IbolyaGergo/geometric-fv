672e56001df550f23d48eef6031d933eb6b87768
IbolyaGergo <ibolyagergi@gmail.com>
2026-03-06 18:34:51 +0100

    Test slope limiter TVD (necessary conditions)

--- code/test/test_slope.py
+++ code/test/test_slope.py
@@ -1,12 +1,14 @@
 import numpy as np
 import pytest
-from hypothesis import given, strategies as st
+from hypothesis import given
+from hypothesis import strategies as st
 from hypothesis.extra.numpy import arrays as hnp
 
 from geometric_fv.config import ReconstConfig
 from geometric_fv.enums import LimiterType, SlopeType
 from geometric_fv.slope import compute_slope
 from geometric_fv.solver import SolverState
+
 
 # TESTs {{{1
 # test_compute_slope_Box() {{{2
@@ -61,6 +63,7 @@ def test_compute_slope_Box_indexing():
 u_st = hnp(np.float64, 3, elements=st.floats(-1e6, 1e6))
 cfl_st = st.floats(0.1, 2.0)
 
+
 @given(u_old=u_st, u_new=u_st, cfl=cfl_st)
 def test_limit_slope_tvd_suff_properties_hypothesis(u_old, u_new, cfl):
     """
@@ -75,8 +78,8 @@ def test_limit_slope_tvd_suff_properties_hypothesis(u_old, u_new, cfl):
     # Calculate theoretical bounds (The "Oracle")
     i = 1
     slope_box = (u_old[i] - u_new[i]) / cfl
-    A = 2.0 * (u_old[i+1] - u_new[i]) / (1.0 + cfl)
-    B = (2.0 / cfl) * (u_old[i] - u_new[i-1]) / (1.0 + cfl)
+    A = 2.0 * (u_old[i + 1] - u_new[i]) / (1.0 + cfl)
+    B = (2.0 / cfl) * (u_old[i] - u_new[i - 1]) / (1.0 + cfl)
 
     # Act
     slope_lim = compute_slope(state, i, u_new[i], reconst_config)
@@ -94,3 +97,37 @@ def test_limit_slope_tvd_suff_properties_hypothesis(u_old, u_new, cfl):
     else:
         assert pytest.approx(slope_lim) == 0.0
 
+
+# test_tvd_necessary_properties_hypothesis() {{{2
+ncells = 4
+u_st = hnp(np.float64, ncells, elements=st.floats(-10, 10))
+slope_st = hnp(np.float64, ncells, elements=st.floats(-10, 10))
+cfl_st = st.floats(0.1, 2.0)
+
+
+@given(u_old=u_st, u_new=u_st, slope=slope_st, cfl=cfl_st)
+def test_limit_slope_tvd_necessary_properties_hypothesis(u_old, u_new, slope, cfl):
+    config_suff = ReconstConfig(
+        slope_type=SlopeType.BOX, limiter_type=LimiterType.TVD_SUFF
+    )
+    config_nec = ReconstConfig(slope_type=SlopeType.BOX, limiter_type=LimiterType.TVD)
+    state = SolverState(u_old=u_old, u_new=u_new, slope=slope, cfl=cfl)
+
+    # Make sure slope[i-1] is properly bounded
+    i = 2
+    slope[i - 1] = compute_slope(state, i - 1, u_new[i - 1], config_suff)
+
+    slope_suff = compute_slope(state, i, u_new[i], config_suff)
+    slope_nec = compute_slope(state, i, u_new[i], config_nec)
+
+    A = 2.0 * (u_old[i + 1] - u_new[i]) / (1.0 + cfl)
+    B = (2.0 / cfl) * (u_old[i] - u_new[i - 1]) / (1.0 + cfl)
+
+    # Downwind limit
+    assert abs(slope_nec) <= abs(A) + 1e-12
+
+    # Necessary should be less restrictive
+    assert abs(slope_nec) >= abs(slope_suff) - 1e-12
+
+    assert np.sign(B) * (-cfl * B + slope[i - 1]) <= np.sign(B) * slope_nec + 1e-12
+    assert np.sign(B) * (B + slope[i - 1]) >= np.sign(B) * slope_nec - 1e-12
