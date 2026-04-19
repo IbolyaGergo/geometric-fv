74c25d1a27360d7e66a8c6c75b522587c5db5119
IbolyaGergo <ibolyagergi@gmail.com>
2026-04-19 17:50:19 +0200

    visual fix (scripts/burgers)
    The types are set in the config variable. This way we don't have to write
    them many times, making the script cleaner.

--- code/scripts/burgers.py
+++ code/scripts/burgers.py
@@ -8,31 +8,38 @@ from geometric_fv.config import (
     ReconstConfig,
     SolverConfig,
 )
-from geometric_fv.enums import BCType, GuessType, LimiterType, SlopeType
+from geometric_fv.enums import (
+    AvgSpeedType,
+    BCType,
+    FluxCorrType,
+    GuessType,
+    LimiterType,
+    SlopeType,
+)
 from geometric_fv.equations import Burgers
 from geometric_fv.schemes import HighResImplicit
 
-# Mesh
-x_min = 0.0
-x_max = 1.5
-ncells = 100
-
-bc_type = BCType.CONSTANT_EXTEND
-slope_type = SlopeType.BOX
-limiter_type = LimiterType.TVD
-guess_type = GuessType.BOX
-
-dt_dx = 1.8
-
 config = SolverConfig(
-    mesh=MeshConfig(x_min=x_min, x_max=x_max, ncells=ncells),
-    boundary=BoundaryConfig(bc_type=bc_type),
-    reconst=ReconstConfig(
-        slope_type=slope_type, limiter_type=limiter_type, guess_type=guess_type
+    mesh=MeshConfig(
+        x_min=0.0,
+        x_max=1.0,
+        ncells=100
     ),
-    iteration=IterationConfig(tol=1e-6, maxiter=50),
+    boundary=BoundaryConfig(
+        bc_type=BCType.CONSTANT_EXTEND
+    ),
+    reconst=ReconstConfig(
+        avg_speed_type = AvgSpeedType.IMPLICIT_UPWIND,
+        flux_corr_type = FluxCorrType.GEOMETRIC,
+        slope_type = SlopeType.BOX,
+        limiter_type = LimiterType.TVD,
+    ),
+    iteration=IterationConfig(
+        tol=1e-6,
+        maxiter=50
+    ),
     equation=Burgers(),
-    dt_dx=dt_dx,
+    dt_dx=1.8,
 )
 
 scheme = HighResImplicit(config=config)
@@ -53,9 +60,9 @@ ncells = mesh.ncells
 # u0 = np.piecewise(
 #     x_c, [x_c < 0.2, (x_c >= 0.2) & (x_c < 0.7), x_c >= 0.7], [0.2, 1, 0.2]
 # )
-u0 = np.piecewise(x_c, [x_c < 0.5, x_c >= 0.5], [1, 0])
+# u0 = np.piecewise(x_c, [x_c < 0.5, x_c >= 0.5], [1, 0])
 # u0 = np.piecewise(x_c, [x_c < 0.5, x_c >= 0.5], [0, 1.0])
-# u0 = 0.05 + 0.95 * np.e**(-50*(x_c - 0.5)**2)
+u0 = 0.05 + 0.95 * np.e**(-50*(x_c - 0.5)**2)
 
 state = scheme.allocate_state(u0)
 
